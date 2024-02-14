7ea877990494244f95026d48dd351191b42c3d37
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-02-14 18:02:41 +0300

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import {defineConfig} from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import {remarkReadingTime} from "./src/remarkReadingTime"
+import vercel from "@astrojs/vercel/serverless"
+import netlify from "@astrojs/netlify/functions"
+import svelte from "@astrojs/svelte"
+
+import sentry from "@sentry/astro"
+import spotlightjs from "@spotlightjs/astro"
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [tailwind(), sitemap(), svelte(), sentry(), spotlightjs()],
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    },
+    output: "server",
+    adapter: vercel(),
+    site: "https://rhythm.nation",
+    server: {
+        port: 3000,
+    },
+})
